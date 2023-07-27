aed94a575df4215883d8e6cb8dbe7726954eb331
Julie-Fabre <julie.m.fabre@gmail.com>
2023-07-27 12:30:47 +0100

    search links

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var articles = [ {
 function hideElements() {
     document.getElementById('home').style.display = 'none';
     //document.getElementById('article').style.display = 'none';
-    document.getElementById('footer').style.display = 'none';
+    //document.getElementById('footer').style.display = 'none';
     //document.body.style.display = 'none';
 }
 
