a2e169aa215a0fb0296be7a289c9e7aede410e86
Julie-Fabre <julie.m.fabre@gmail.com>
2023-07-27 11:07:32 +0100

    search bar enter

--- script.js
+++ script.js
@@ -2,16 +2,24 @@
 var searchForm = document.getElementById('searchForm');
 var input = document.getElementById('search');
 
+// Add keyup event listener to the search input field
+input.addEventListener('keyup', function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        searchPosts();
+    }
+});
+
 // Add submit event listener to the search form
 searchForm.addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault();
     searchPosts();
 });
 
 function searchPosts() {
     var filter = input.value.toUpperCase();
-    var section = document.getElementById("blog");
-    var articles = section.getElementsByTagName('article');
+    //var section = document.getElementById("blog");
+    var articles = document.getElementsByTagName('article');
 
     for (var i = 0; i < articles.length; i++) {
         var h3 = articles[i].getElementsByTagName("h3")[0];
