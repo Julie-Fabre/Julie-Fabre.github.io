42dad6ac5fd245fc0a59ea98300c03220ec18f79
Julie-Fabre <julie.m.fabre@gmail.com>
2023-07-27 11:37:43 +0100

    search bar enter

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-// JavaScript
-function searchPosts() {
-    var filter = document.getElementById('search').value.toUpperCase();
-    var section = document.getElementById("blog");
-    var articles = section.getElementsByTagName('article');
+// Assuming articles is an array of objects with 'title' and 'content' properties
+var articles = [ {
+        title: 'Tintin''s rocket',
+        content: 'Web development has its roots in the early days of the internet...',
+        date: '2023-07-27'
+    },];
 
-    for (var i = 0; i < articles.length; i++) {
-        var h3 = articles[i].getElementsByTagName("h3")[0];
-        var txtValue = h3.textContent || h3.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            articles[i].style.display = "";
-        } else {
-            articles[i].style.display = "none";
-        }
+function searchArticles() {
+    var query = document.getElementById('search').value;
+
+    // Clear current search results
+    document.getElementById('searchResults').innerHTML = '';
+
+    // Filter the articles based on the user's query
+    var results = articles.filter(function(article) {
+        return article.title.includes(query) || article.content.includes(query);
+    });
+
+    // Display the results
+    for (var i = 0; i < results.length; i++) {
+        var resultDiv = document.createElement('div');
+        var title = document.createElement('h2');
+        var content = document.createElement('p');
+
+        title.textContent = results[i].title;
+        content.textContent = results[i].content;
+
+        resultDiv.appendChild(title);
+        resultDiv.appendChild(content);
+
+        document.getElementById('searchResults').appendChild(resultDiv);
     }
-
-    return false; // Prevent form submission
 }
